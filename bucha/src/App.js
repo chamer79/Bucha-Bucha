c3461e9e05f9f5b93c3508d26581bb1500e5b342
Carri-anne Hamer <chamer79@gmail.com>
2021-05-08 13:46:31 -0400

    nav

--- bucha/src/App.js
+++ bucha/src/App.js
@@ -1,10 +1,20 @@
-
+import { baseURL, config } from "./services";
+import axios from "axios";
 import './App.css';
+import { Route } from "react-router-dom";
+import { useEffect } from "react";
+import Nav from "./components/Nav";
 
 function App() {
+  
+
+
   return (
     <div className="App">
-     
+      <Nav />
+      <Route exact path="/">
+
+      </Route>
     </div>
   );
 }
---
+++ bucha/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <section>
+      <p> &copy; Bucha Bucha | Carri-anne Hamer 2021</p>
+    </section>
+  )
+}
+
+export default Footer;
