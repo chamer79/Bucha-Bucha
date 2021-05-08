64966b870d43e2cae86e9527390179638544e9fc
Carri-anne Hamer <chamer79@gmail.com>
2021-05-08 16:18:31 -0400

    footer.jsx & app.js

--- bucha/src/App.js
+++ bucha/src/App.js
@@ -1,20 +1,38 @@
 import { baseURL, config } from "./services";
+import { Menu, Dropdown } from "antd";
+import { DownOutlined } from '@ant-design/icons';
+import { useEffect } from "react";
 import axios from "axios";
 import './App.css';
-import { Route } from "react-router-dom";
-import { useEffect } from "react";
+import { Link, Route } from "react-router-dom";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer"
 
 function App() {
-  
+  //=================================
+  //  GET Request
+  //=================================
 
 
+  //=================================
+  //  Passing Nav & Footer
+  // ------------------------
+  //  Creating Button  Importing
+  //   buttons 
+  //=================================
   return (
     <div className="App">
       <Nav />
-      <Route exact path="/">
-
-      </Route>
+      <Route exact path="/"></Route>
+      <main>
+        <h2>Flavor. Ferment. Enjoy.</h2>
+        <p>Welcome! Bucha Bucha is a community where people can share, get inspired, or experiment with various recipes to flavor home brewed kombuchas.</p>
+        <div>
+ 
+          <button>Bucha Basics</button>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
--- bucha/src/components/Nav.jsx
+++ bucha/src/components/Nav.jsx
@@ -2,9 +2,16 @@ import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav>
-      <Link to="/">BUCHA BUCHA</Link>
-    </nav>
+    <header className="header">
+      {/* Header image */}
+      {/* <img src="https://images.unsplash.com/photo-1583396216852-1e1d61137170?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+        alt="kombucha brewing" /> */}
+      <nav>
+        <Link to="/">
+          <h1>BUCHA BUCHA</h1>
+        </Link>
+      </nav>
+    </header>
   )
 }
 
